30b419113c8af36843ab9fe523b262a0a0863f44
Amir Pourhadi <AmirPourhadi@outlook.com>
2022-01-04 11:30:24 +0330

    Create a socket connection

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const socket = require("socket.io");
 const app = express();
 
 // Define the port to run on
 const port = process.env.PORT || 8080;
 
+// Listen to the port
+const server = app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
+
 // Serve static files
 app.use(express.static("public"));
 
-// Listen to the port
-app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
+// Socket setup
+const io = socket(server);
+
+io.on("connection", (socket) => {
+  console.log("made socket connection");
+});
