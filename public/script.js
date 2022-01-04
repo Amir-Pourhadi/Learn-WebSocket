11aad659687f1c4648bae0dd3ab002ff9c63310b
Amir Pourhadi <AmirPourhadi@outlook.com>
2022-01-04 11:37:07 +0330

    Emit sending message

--- public/script.js
+++ public/script.js
@@ -6,3 +6,8 @@ const message = document.getElementById("message"),
   handle = document.getElementById("handle"),
   btn = document.getElementById("send"),
   output = document.getElementById("output");
+
+// Emit events
+btn.addEventListener("click", () => {
+  socket.emit("chat", { message: message.value, handle: handle.value });
+});
