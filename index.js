2e9fc31b6f1f22ff45d0626c40c5bf9f76957fbe
Amir Pourhadi <AmirPourhadi@outlook.com>
2022-01-04 10:42:01 +0330

    Create server

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+// Define the port to run on
+const port = process.env.PORT || 8080;
+
+// Listen to the port
+app.listen(port, () => console.log(`Listening on port ${port}`));
