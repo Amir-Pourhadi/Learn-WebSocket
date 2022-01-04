120d402828db801b71fbf4f88220934d21bd9c16
Amir Pourhadi <AmirPourhadi@outlook.com>
2022-01-04 11:32:34 +0330

    Select dom

--- public/script.js
+++ public/script.js
@@ -1,2 +1,8 @@
 // Make socket connection
 const socket = io.connect("http://localhost:8080");
+
+// Query DOM
+const message = document.getElementById("message"),
+  handle = document.getElementById("handle"),
+  btn = document.getElementById("send"),
+  output = document.getElementById("output");
