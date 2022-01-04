4ea5e26672cc0c4f0b51077fdf231090457249f5
Amir Pourhadi <AmirPourhadi@outlook.com>
2022-01-04 11:48:08 +0330

    Get data from server

--- public/script.js
+++ public/script.js
@@ -11,3 +11,8 @@ const message = document.getElementById("message"),
 btn.addEventListener("click", () => {
   socket.emit("chat", { message: message.value, handle: handle.value });
 });
+
+// Listen for events
+socket.on("chat", (data) => {
+  output.innerHTML += `<p><strong>${data.handle}:</strong> ${data.message}</p>`;
+});
