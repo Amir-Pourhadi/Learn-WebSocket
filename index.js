5096908561d0bf6b6b319e0ca0dbe2e73b2702c6
Amir Pourhadi <AmirPourhadi@outlook.com>
2022-01-04 10:46:18 +0330

    Serve static files

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ const app = express();
 // Define the port to run on
 const port = process.env.PORT || 8080;
 
+// Serve static files
+app.use(express.static("public"));
+
 // Listen to the port
 app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
